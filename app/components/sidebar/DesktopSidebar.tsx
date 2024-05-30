192008a8d123d4fe0e71fb167fa72be3b30fd4b8
thieutq <thieutq@example.com>
2024-05-30 10:18:48 +0700

    feat: user setting

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -5,6 +5,7 @@ import React, { use, useState } from 'react';
 import DesktopItem from './DesktopItem';
 import { User } from '@prisma/client';
 import Avatar from '../Avatar';
+import SettingsModal from './SettingsModal';
 
 type Props = {
   currentUser: User;
@@ -15,73 +16,76 @@ const DesktopSidebar: React.FC<Props> = ({ currentUser }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div
-      className='
-        hidden
-        lg:fixed
-        lg:inset-y-0
-        lg:left-0
-        lg:z-40
-        lg:w-20
-        lg:px-6
-        lg:overflow-y-auto
-        lg:bg-white
-        lg:border-r-[1px]
-        lg:pb-4
-        lg:flex
-        lg:flex-col
-        lg:justify-between
-      '
-    >
-      <nav
+    <>
+      <SettingsModal
+        currentUser={currentUser}
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      />
+      <div
         className='
-          mt-4
-          flex
-          flex-col
-          justify-between
+          hidden
+          lg:fixed
+          lg:inset-y-0
+          lg:left-0
+          lg:z-40
+          lg:w-20
+          lg:px-6
+          lg:overflow-y-auto
+          lg:bg-white
+          lg:border-r-[1px]
+          lg:pb-4
+          lg:flex
+          lg:flex-col
+          lg:justify-between
         '
       >
-        <ul
-          role='list'
+        <nav
           className='
+            mt-4
             flex
             flex-col
-            items-center
-            space-y-1
+            justify-between
           '
         >
-          {routers.map((item) => (
-            <DesktopItem
-              key={item.label}
-              href={item.href}
-              label={item.label}
-              icon={item.icon}
-              active={item.active}
-              onClick={item.onClick}
-            />
-          ))}
-        </ul>
-      </nav>
-      <nav
-        className='
-          mt-4
-          flex
-          flex-col
-          items-center
-          justify-between
-        '
-      >
-        <div
+          <ul
+            role='list'
+            className='
+              flex
+              flex-col
+              items-center
+              space-y-1
+            '
+          >
+            {routers.map((item) => (
+              <DesktopItem
+                key={item.label}
+                href={item.href}
+                label={item.label}
+                icon={item.icon}
+                active={item.active}
+                onClick={item.onClick}
+              />
+            ))}
+          </ul>
+        </nav>
+        <nav
           onClick={() => setIsOpen(true)}
           className='
+            mt-4
+            flex
+            flex-col
+            justify-between
+            items-center
             cursor-pointer
             hover:opacity-75
             transition
           '
-        />
+        >
           <Avatar user={currentUser}/>
-      </nav>
-    </div>
+        </nav>
+      </div>
+    </>
   );
 };
 
